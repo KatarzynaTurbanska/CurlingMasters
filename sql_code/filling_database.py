4ebd6356eb8a12615413ff0c52f4d33063d53af7
3dytalabedzka <edlabedzka@gmail.com>
2022-06-19 14:27:32 +0200

    Fixed connector

--- sql_code/filling_database.py
+++ sql_code/filling_database.py
@@ -1,8 +1,9 @@
 import mysql.connector as mysql
 import pandas as pd
+from config import host, user, password
 
-def fill_table(table_name, query, password):
-    conn = password
+def fill_table(table_name, query , user, password, host):
+    conn = mysql.connect(user=user, password=password, host=host, database='team3')
     data = pd.read_csv('../final_data/{}.csv'.format(table_name))
     data = data.where(pd.notnull(data), None)
 
@@ -14,16 +15,16 @@ def fill_table(table_name, query, password):
     print("Records inserted")
 
 if __name__ == '__main__':
-    fill_table('gender', '(%s,%s)')
-    fill_table('people', '(%s,%s,%s)')
-    fill_table('phone_book', '(%s,%s)')
-    fill_table('finances', '(%s,%s,%s)')
-    fill_table('address_book', '(%s,%s,%s,%s)')
-    fill_table('address', '(%s,%s)')
-    fill_table('facility', '(%s,%s)')
-    fill_table('equipment', '(%s,%s,%s,%s)')
-    fill_table('teams', '(%s,%s,%s,%s)')
-    fill_table('personal_info', '(%s,%s,%s,%s,%s,%s)')
-    fill_table('matches', '(%s,%s,%s,%s,%s,%s,%s)')
-    fill_table('opponents', '(%s,%s,%s)')
-    fill_table('positions', '(%s,%s,%s)')
+    fill_table('gender', '(%s,%s)', user, password, host)
+    fill_table('people', '(%s,%s,%s)', user, password, host)
+    fill_table('phone_book', '(%s,%s)', user, password, host)
+    fill_table('finances', '(%s,%s,%s)', user, password, host)
+    fill_table('address_book', '(%s,%s,%s,%s)', user, password, host)
+    fill_table('address', '(%s,%s)', user, password, host)
+    fill_table('facility', '(%s,%s)', user, password, host)
+    fill_table('equipment', '(%s,%s,%s,%s)', user, password, host)
+    fill_table('teams', '(%s,%s,%s,%s)', user, password, host)
+    fill_table('personal_info', '(%s,%s,%s,%s,%s,%s)', user, password, host)
+    fill_table('matches', '(%s,%s,%s,%s,%s,%s,%s)', user, password, host)
+    fill_table('opponents', '(%s,%s,%s)', user, password, host)
+    fill_table('positions', '(%s,%s,%s)', user, password, host)
